844e347c17df5928f62ea61c55525bf8105c32e8
MasahiroKaga <masahiro41802891@gmail.com>
2022-01-19 15:24:21 -0800

    Set up tailwind css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Fragment } from "react";
 import "./App.css";
+import Layout from "./components/Layout";
 
 function App() {
-  return <div>
-    
-  </div>;
+  return (
+    <Fragment>
+      <Layout></Layout>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </Fragment>
+  );
 }
 
 export default App;

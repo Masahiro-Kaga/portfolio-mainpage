f541e2814fa1b7434c4c91565aa8b732c5b97653
MasahiroKaga <masahiro41802891@gmail.com>
2022-01-20 00:45:23 -0800

    Install daisyUI as a component UI and delete other css files to debug

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { Fragment } from "react";
-import "./App.css";
+import { Fragment} from "react";
 import Layout from "./components/Layout";
+
 
 function App() {
   return (
     <Fragment>
       <Layout></Layout>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      {/* <h1 class="text-3xl font-bold underline">Hello world!</h1> */}
+      
     </Fragment>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 module.exports = {
+  daisyui: {
+    // styled: true,
+    themes: false,
+    // rtl: false,
+  },
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 }

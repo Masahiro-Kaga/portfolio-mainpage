70b62263945bbb4e9a5d5f699959a8de404249af
MasahiroKaga <masahiro41802891@gmail.com>
2022-01-23 13:32:28 -0800

    Optimized Navbar

--- src/components/Header/Navigation.js
+++ src/components/Header/Navigation.js
@@ -11,8 +11,8 @@ const Navigation = () => {
   };
 
   return (
-    <div style={{background:'#white'}}>
-      <nav className=" px-10 sm:px-20 py-5 fixed top-0 left-0 w-full">
+    <div style={{ background: "#white" }}>
+      <nav className="px-10 sm:px-20 py-5 fixed top-0 left-0 w-full">
         <div className="container flex flex-wrap justify-between items-center mx-auto">
           <a href="#" className="flex">
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white font-dosis">
@@ -23,7 +23,7 @@ const Navigation = () => {
           <div className="relative">
             <button
               type="button"
-              className="md:hidden text-gray-500 w-10 h-10 relative rounded-lg focus:outline-none bg-slate-500"
+              className="md:hidden text-gray-500 w-10 h-10 relative rounded-lg focus:outline-none bg-gray-800 border-2" 
               onClick={toggleDropdownHandler}
             >
               <div className="block w-5 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 m-auto">
@@ -49,7 +49,7 @@ const Navigation = () => {
             </button>
             <ul
               id="hiddenClass"
-              className=" min-w-max absolute right-0 text-base z-50 float-right py-2 list-none text-left rounded shadow-lg mt-1 hidden m-0 bg-clip-padding border-none bg-gray-800"
+              className="min-w-max absolute right-0 text-base z-50 float-right py-2 list-none text-left rounded-lg shadow-lg mt-1 hidden m-0 bg-clip-padding border-none bg-gray-800"
               aria-labelledby="dropdownMenuButton"
             >
               <li>
@@ -57,7 +57,7 @@ const Navigation = () => {
                   className="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-300 hover:bg-gray-700 hover:text-white focus:text-white focus:bg-gray-700 active:bg-blue-600"
                   href="#"
                 >
-                  Action
+                  Work
                 </a>
               </li>
               <li>
@@ -65,26 +65,25 @@ const Navigation = () => {
                   className="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-300 hover:bg-gray-700 hover:text-white focus:text-white focus:bg-gray-700"
                   href="#"
                 >
-                  Another action
+                  Contact
                 </a>
               </li>
-              <li>
-                <a
-                  className="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-300 hover:bg-gray-700 hover:text-white focus:text-white focus:bg-gray-700"
-                  href="#"
-                >
-                  Something else here
-                </a>
-              </li>
+              
             </ul>
           </div>
           <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
             <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-              {content.nav.links.map((link,index)=>{
-                return <li>
-                  <a href="#"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">{link.text}</a>
-                </li>
+              {content.nav.links.map((link, index) => {
+                return (
+                  <li>
+                    <a
+                      href="#"
+                      className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-700 rounded-xl"
+                    >
+                      {link.text}
+                    </a>
+                  </li>
+                );
               })}
             </ul>
           </div>

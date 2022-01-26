e7b4576ec2d3dbca8d217298856c06ba498a724f
MasahiroKaga <masahiro41802891@gmail.com>
2022-01-25 18:32:06 -0800

    Create About Content component

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import content from '../content';
+import content from '../content/index.js';
 
 const AboutContent = () => {
   return <div>
-      <div className="min-h-screen w-full flex">
+      <div className="min-h-screen w-full flex flex-col justify-center items-center">
       
-      <h1 className=" text-white">{content.header.text[0]}</h1>
-
+      <p className="font-dosis font-bold text-4xl">About this site</p>
+      <br/>
+      <p className='sm:w-[50vw] mx-3 tracking-tight text-center'>Thank you for your visit. Here is Masa's portfolio site. I showcase my masterpieces and my skill sets with my effort.I would like you to know more about me, this is what I really do.Please be my guest and take your time.</p>
+      <br/>
+      <p className='md:w-[50vw] mx-3 tracking-tight text-center'>Would you like to have some tea?</p>
+      
     </div>
   </div>;
 };

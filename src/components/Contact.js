6ac4e2892bbf1f757d99e42be3b0cddc5b945c04
MasahiroKaga <masahiro41802891@gmail.com>
2022-02-20 10:40:19 -0800

    Try to debug,not submitted comment 6

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,12 +2,50 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <form name="contact" method="POST" netlify data-netlify="true">
-      <input type="text" name="name" placeholder="Name" />
-      <input type="email" name="email" placeholder="Email" />
-      <textarea name="message" placeholder="Message"></textarea>
-      <div class="form-btn">
-        <button type="submit">SUBMIT</button>
+    <form name="contact-form" method="POST" data-netlify="true" className="bg-gray-800 text-gray-100 px-8 py-12">
+      <input type="hidden" name="form-name" value="contact-form"></input>
+      <div className="text-center w-full">
+      <p className="font-dosis font-bold text-4xl">Contact Me</p>
+      </div>
+      <div className="max-w-screen-xl mt-24 px-8 grid gap-8 grid-cols-1 md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 py-16 mx-auto bg-gray-100 text-gray-900 rounded-lg shadow-lg">
+        <div className="flex flex-col justify-between">
+          <div>
+            <h2 className="text-4xl lg:text-5xl font-bold leading-tight">
+              Lets talk about everything Yay!
+            </h2>
+          </div>
+          <div className="mt-8 text-center"></div>
+        </div>
+        <div className="">
+          <div>
+            <span className="uppercase text-sm text-gray-600 font-bold">
+              Name
+            </span>
+            <input
+              className="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:shadow-outline"
+              type="text"
+              placeholder=""
+              name="name"
+              required
+            ></input>
+          </div>
+          <div className="mt-8">
+            <span className="uppercase text-sm text-gray-600 font-bold">Email</span>
+            <input
+              className="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:shadow-outline"
+              type="text"
+              placeholder=""
+              name="email"
+              required
+            ></input>
+          </div>
+          
+          <div className="mt-8">
+            <button type="submit" className="uppercase text-sm font-bold tracking-wide bg-indigo-500 text-gray-100 p-3 rounded-lg w-full focus:shadow-outline">
+              Send Message
+            </button>
+          </div>
+        </div>
       </div>
     </form>
   );

2f0a11b09b90096ad7b77e129a59f95add87d094
MasahiroKaga <masahiro41802891@gmail.com>
2022-02-20 10:30:31 -0800

    Try to debug,not submitted comment 5

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,13 +2,14 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <form name="contact" method="POST" netlify data-netlify="true" data-netlify-honeypot="bot-field">
-  <input type="hidden" name="form-name" value="contact" />
-    <input type="text" name="name" placeholder="Name"/>   
-    <input type="email" name="email" placeholder="Email"/>
-    <textarea name="message" placeholder="Message"></textarea>
-    <div class="form-btn"><button type="submit">SUBMIT</button></div>
-</form>
+    <form name="contact" method="POST" netlify data-netlify="true">
+      <input type="text" name="name" placeholder="Name" />
+      <input type="email" name="email" placeholder="Email" />
+      <textarea name="message" placeholder="Message"></textarea>
+      <div class="form-btn">
+        <button type="submit">SUBMIT</button>
+      </div>
+    </form>
   );
 };
 

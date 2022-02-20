666a4f969d6551396647d8a523fcb695f19b2091
MasahiroKaga <masahiro41802891@gmail.com>
2022-02-20 10:11:39 -0800

    Try to debug,not submitted comment 3

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,58 +2,31 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <form name="contact-form" method="POST" data-netlify="true" className="bg-gray-800 text-gray-100 px-8 py-12">
-      <div className="text-center w-full">
-      <p className="font-dosis font-bold text-4xl">Contact Me</p>
-      </div>
-      <div className="max-w-screen-xl mt-24 px-8 grid gap-8 grid-cols-1 md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 py-16 mx-auto bg-gray-100 text-gray-900 rounded-lg shadow-lg">
-        <div className="flex flex-col justify-between">
-          <div>
-            <h2 className="text-4xl lg:text-5xl font-bold leading-tight">
-              Lets talk about everything Yay!
-            </h2>
-          </div>
-          <div className="mt-8 text-center"></div>
-        </div>
-        <div className="">
-          <div>
-            <span className="uppercase text-sm text-gray-600 font-bold">
-              Name
-            </span>
-            <input
-              className="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:shadow-outline"
-              type="text"
-              placeholder=""
-              name="name"
-              required
-            ></input>
-          </div>
-          <div className="mt-8">
-            <span className="uppercase text-sm text-gray-600 font-bold">Email</span>
-            <input
-              className="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:shadow-outline"
-              type="text"
-              placeholder=""
-              name="email"
-              required
-            ></input>
-          </div>
-          <div className="mt-8">
-            <span className="uppercase text-sm text-gray-600 font-bold">
-              Message
-            </span>
-            <textarea className="w-full h-32 bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:shadow-outline"
-              placeholder=""
-              name="comment"
-              required></textarea>
-          </div>
-          <div className="mt-8">
-            <button className="uppercase text-sm font-bold tracking-wide bg-indigo-500 text-gray-100 p-3 rounded-lg w-full focus:shadow-outline">
-              Send Message
-            </button>
-          </div>
-        </div>
-      </div>
+    <form
+      className="contact-form"
+      name="contact"
+      method="POST"
+      action="/success/"
+      data-netlify="true"
+      netlify-honeypot="bot-field"
+    >
+      <input type="hidden" name="bot-field" />
+      <input type="hidden" name="form-name" value="contact" />
+      <label>
+        Name*
+        <input aria-required="true" type="text" id="name" name="name" />
+      </label>
+      <label>
+        Email*
+        <input aria-required="true" type="email" id="email" name="email" />
+      </label>
+      <label>
+        Message*
+        <textarea aria-required="true" id="message" name="message"></textarea>
+      </label>
+      <button type="submit" className="button button--primary">
+        Submit
+      </button>
     </form>
   );
 };
